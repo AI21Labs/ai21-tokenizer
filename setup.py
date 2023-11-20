77ee751b6d7610c4b4955ad488579a88f47d4f8b
asafgardin <147075902+asafgardin@users.noreply.github.com>
2023-11-20 10:53:28 +0200

    feat: Pypi publish (#18)
    * feast: Added setup.py
    
    * feast: Added publish.yaml

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from version import __version__
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\\n" + fh.read()
+
+setup(
+    name="ai21_tokenizer",
+    version=__version__,
+    author="AI21 Labs",
+    author_email="support@ai21.com",
+    description="Library for tokenizing text using AI21 Labs' Jurassic tokenizer",
+    url="https://github.com/AI21Labs/ai21_tokenizer",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(exclude=["tests", "tests.*", "ci_scripts"]),
+    install_requires=[],
+    keywords=["python", "tokenizer", "ai", "ai21", "jurassic"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
