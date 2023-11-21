2f37b188ec7ff5fe7dcc75f9b3a3d56b218bb81a
Asaf Gardin <asafg@ai21.com>
2023-11-21 12:33:05 +0200

    fix: assert in example

--- examples/use_tokenizer.py
+++ examples/use_tokenizer.py
@@ -5,4 +5,4 @@ example_sentence = "This sentence should be encoded and then decoded. Hurray!"
 encoded = tokenizer.encode(example_sentence)
 decoded = tokenizer.decode(encoded)
 
-print(decoded == example_sentence)
+assert decoded == example_sentence
