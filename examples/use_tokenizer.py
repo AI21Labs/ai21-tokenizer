a471661f0307b43860cc907072e48752138dc5a0
Asaf Gardin <asafg@ai21.com>
2023-11-21 17:38:37 +0200

    fix: test 2

--- examples/use_tokenizer.py
+++ examples/use_tokenizer.py
@@ -1,7 +1,7 @@
 from ai21_tokenizer import Tokenizer
 
 tokenizer = Tokenizer.get_tokenizer()
-example_sentence = "This sentence should be encoded and then decoded. Hurray!"
+example_sentence = "This sentence should be encoded and then decoded. Hurray!!"
 encoded = tokenizer.encode(example_sentence)
 decoded = tokenizer.decode(encoded)
 
