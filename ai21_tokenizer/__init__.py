3cc71e747a0b5d5450dbe70618456d415171ae23
asafgardin <147075902+asafgardin@users.noreply.github.com>
2023-12-27 16:33:48 +0200

    fix: BaseTokenizer in init (#68)

--- ai21_tokenizer/__init__.py
+++ ai21_tokenizer/__init__.py
@@ -1,7 +1,8 @@
-from .version import VERSION
-from ai21_tokenizer.tokenizer_factory import TokenizerFactory as Tokenizer
+from ai21_tokenizer.base_tokenizer import BaseTokenizer
 from ai21_tokenizer.jurassic_tokenizer import JurassicTokenizer
+from ai21_tokenizer.tokenizer_factory import TokenizerFactory as Tokenizer
+from .version import VERSION
 
 __version__ = VERSION
 
-__all__ = ["Tokenizer", "JurassicTokenizer", "__version__"]
+__all__ = ["Tokenizer", "JurassicTokenizer", "BaseTokenizer", "__version__"]
