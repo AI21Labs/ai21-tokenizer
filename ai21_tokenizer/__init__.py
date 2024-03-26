7e37fd0204ea195274b66a66dfb8879598081b32
Asaf Gardin <asafg@ai21.com>
2024-03-26 16:27:58 +0200

    refactor: Added import to init

--- ai21_tokenizer/__init__.py
+++ ai21_tokenizer/__init__.py
@@ -1,8 +1,8 @@
 from ai21_tokenizer.base_tokenizer import BaseTokenizer
 from ai21_tokenizer.jurassic_tokenizer import JurassicTokenizer
-from ai21_tokenizer.tokenizer_factory import TokenizerFactory as Tokenizer
+from ai21_tokenizer.tokenizer_factory import TokenizerFactory as Tokenizer, PreTrainedTokenizers
 from .version import VERSION
 
 __version__ = VERSION
 
-__all__ = ["Tokenizer", "JurassicTokenizer", "BaseTokenizer", "__version__"]
+__all__ = ["Tokenizer", "JurassicTokenizer", "BaseTokenizer", "__version__", "PreTrainedTokenizers"]
