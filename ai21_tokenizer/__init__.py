b8dfc841520d394ff3f2a0bab18dc03baa501917
asafg <asafg@ai21.com>
2024-03-27 13:10:07 +0200

    fix: Added JambaInstructTokenizer to main init

--- ai21_tokenizer/__init__.py
+++ ai21_tokenizer/__init__.py
@@ -1,8 +1,16 @@
 from ai21_tokenizer.base_tokenizer import BaseTokenizer
+from ai21_tokenizer.jamba_instruct_tokenizer import JambaInstructTokenizer
 from ai21_tokenizer.jurassic_tokenizer import JurassicTokenizer
 from ai21_tokenizer.tokenizer_factory import TokenizerFactory as Tokenizer, PreTrainedTokenizers
 from .version import VERSION
 
 __version__ = VERSION
 
-__all__ = ["Tokenizer", "JurassicTokenizer", "BaseTokenizer", "__version__", "PreTrainedTokenizers"]
+__all__ = [
+    "Tokenizer",
+    "JurassicTokenizer",
+    "BaseTokenizer",
+    "__version__",
+    "PreTrainedTokenizers",
+    "JambaInstructTokenizer",
+]
