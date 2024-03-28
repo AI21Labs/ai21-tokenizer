9d193c972a17bdfb3f59c3fa83f1a1036e57b7f7
asafg <asafg@ai21.com>
2024-03-28 15:58:15 +0200

    feat: Used hf path to tokenizer

--- ai21_tokenizer/tokenizer_factory.py
+++ ai21_tokenizer/tokenizer_factory.py
@@ -7,11 +7,12 @@ from ai21_tokenizer.jurassic_tokenizer import JurassicTokenizer
 
 _LOCAL_RESOURCES_PATH = Path(__file__).parent / "resources"
 _ENV_CACHE_DIR_KEY = "AI21_TOKENIZER_CACHE_DIR"
+JAMABA_TOKENIZER_HF_PATH = "ai21labs/Jamba-v0.1"
 
 
 class PreTrainedTokenizers:
     J2_TOKENIZER = "j2-tokenizer"
-    JAMBA_TOKENIZER = "jamba-tokenizer"
+    JAMBA_INSTRUCT_TOKENIZER = "jamba-instruct-tokenizer"
 
 
 class TokenizerFactory:
@@ -22,10 +23,8 @@ class TokenizerFactory:
 
     @classmethod
     def get_tokenizer(cls, tokenizer_name: str = PreTrainedTokenizers.J2_TOKENIZER) -> BaseTokenizer:
-        if tokenizer_name == PreTrainedTokenizers.JAMBA_TOKENIZER:
-            return JambaInstructTokenizer(
-                model_path="ai21labs/jamba-instruct-tokenizer-test", cache_dir=os.getenv(_ENV_CACHE_DIR_KEY)
-            )
+        if tokenizer_name == PreTrainedTokenizers.JAMBA_INSTRUCT_TOKENIZER:
+            return JambaInstructTokenizer(model_path=JAMABA_TOKENIZER_HF_PATH, cache_dir=os.getenv(_ENV_CACHE_DIR_KEY))
 
         if tokenizer_name == PreTrainedTokenizers.J2_TOKENIZER:
             return JurassicTokenizer(_LOCAL_RESOURCES_PATH / PreTrainedTokenizers.J2_TOKENIZER)
