262afe83282cbd4ef55ee4662bd09817ce418d48
asafg <asafg@ai21.com>
2024-03-28 18:01:43 +0200

    fix: CR

--- ai21_tokenizer/jamba_instruct_tokenizer.py
+++ ai21_tokenizer/jamba_instruct_tokenizer.py
@@ -6,22 +6,21 @@ from pathlib import Path
 from typing import Union, List, cast, Optional
 
 from tokenizers import Tokenizer
-from tokenizers import Tokenizer as TokenizerType
 
 from ai21_tokenizer import BaseTokenizer
 from ai21_tokenizer.utils import PathLike
 
 _TOKENIZER_FILE = "tokenizer.json"
-_DEFAULT_MODEL_CACHE_DIR = Path(tempfile.gettempdir()) / "tokenizer_cache"
+_DEFAULT_MODEL_CACHE_DIR = Path(tempfile.gettempdir()) / "jamba_instruct"
 
 
 class JambaInstructTokenizer(BaseTokenizer):
-    _tokenizer: TokenizerType
+    _tokenizer: Tokenizer
 
     def __init__(
         self,
         model_path: str,
-        cache_dir: Optional[PathLike] = _DEFAULT_MODEL_CACHE_DIR,
+        cache_dir: Optional[PathLike] = None,
     ):
         """
         Args:
@@ -33,22 +32,25 @@ class JambaInstructTokenizer(BaseTokenizer):
         """
         self._tokenizer = self._init_tokenizer(model_path=model_path, cache_dir=cache_dir or _DEFAULT_MODEL_CACHE_DIR)
 
-    def _init_tokenizer(self, model_path: PathLike, cache_dir: PathLike) -> TokenizerType:
-        if Path(cache_dir).exists() and _TOKENIZER_FILE in os.listdir(cache_dir):
+    def _init_tokenizer(self, model_path: PathLike, cache_dir: PathLike) -> Tokenizer:
+        if self._is_cached(cache_dir):
             return self._load_from_cache(cache_dir / _TOKENIZER_FILE)
 
         tokenizer = cast(
-            TokenizerType,
+            Tokenizer,
             Tokenizer.from_pretrained(model_path),
         )
         self._cache_tokenizer(tokenizer, cache_dir)
 
         return tokenizer
 
-    def _load_from_cache(self, cache_file: Path) -> TokenizerType:
-        return cast(TokenizerType, Tokenizer.from_file(str(cache_file)))
+    def _is_cached(self, cache_dir: PathLike) -> bool:
+        return Path(cache_dir).exists() and _TOKENIZER_FILE in os.listdir(cache_dir)
 
-    def _cache_tokenizer(self, tokenizer: TokenizerType, cache_dir: PathLike) -> None:
+    def _load_from_cache(self, cache_file: Path) -> Tokenizer:
+        return cast(Tokenizer, Tokenizer.from_file(str(cache_file)))
+
+    def _cache_tokenizer(self, tokenizer: Tokenizer, cache_dir: PathLike) -> None:
         # create cache directory for caching the tokenizer and save it
         Path(cache_dir).mkdir(parents=True, exist_ok=True)
         tokenizer.save(str(cache_dir / _TOKENIZER_FILE))
