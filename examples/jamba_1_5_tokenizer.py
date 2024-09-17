d5eeb96a0c9e659c40f428e57c12f95cccc7f306
miri-bar <160584887+miri-bar@users.noreply.github.com>
2024-09-17 15:41:04 +0300

    docs: add note about jamba 1.5s (#100)
    * docs: add note about jamba 1.5s
    
    * ci: add hf token from secrets for tests

--- examples/jamba_1_5_tokenizer.py
+++ examples/jamba_1_5_tokenizer.py
@@ -1,5 +1,12 @@
 from ai21_tokenizer import Jamba1_5Tokenizer
 
+"""
+If you wish to use the tokenizers for `Jamba 1.5 Mini` or `Jamba 1.5 Large`,
+you will need to request access to the relevant model's HuggingFace repo:
+* https://huggingface.co/ai21labs/AI21-Jamba-1.5-Mini
+* https://huggingface.co/ai21labs/AI21-Jamba-1.5-Large
+"""
+
 model_path = "ai21labs/AI21-Jamba-1.5-Mini"
 
 tokenizer = Jamba1_5Tokenizer(model_path=model_path)
--- examples/use_tokenizer.py
+++ examples/use_tokenizer.py
@@ -1,5 +1,12 @@
 from ai21_tokenizer import Tokenizer
 
+"""
+If you wish to use the tokenizers for `Jamba 1.5 Mini` or `Jamba 1.5 Large`,
+you will need to request access to the relevant model's HuggingFace repo:
+* https://huggingface.co/ai21labs/AI21-Jamba-1.5-Mini
+* https://huggingface.co/ai21labs/AI21-Jamba-1.5-Large
+"""
+
 tokenizer = Tokenizer.get_tokenizer()
 example_sentence = "This sentence should be encoded and then decoded. Hurray!!"
 encoded = tokenizer.encode(example_sentence)
