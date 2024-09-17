f39dcb2b8f407b3458d4305b6e280b32d564f830
Miri Bar <mirib@ai21.com>
2024-09-17 15:05:09 +0300

    docs: add note about jamba 1.5s

--- examples/use_async_tokenizer.py
+++ examples/use_async_tokenizer.py
@@ -2,6 +2,13 @@ import asyncio
 
 from ai21_tokenizer import Tokenizer
 
+"""
+If you wish to use the tokenizers for `Jamba 1.5 Mini` or `Jamba 1.5 Large`,
+you will need to request access to the relevant model's HuggingFace repo:
+* https://huggingface.co/ai21labs/AI21-Jamba-1.5-Mini
+* https://huggingface.co/ai21labs/AI21-Jamba-1.5-Large
+"""
+
 
 async def main():
     tokenizer = await Tokenizer.get_async_tokenizer()
